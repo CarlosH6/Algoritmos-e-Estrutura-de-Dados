d9e06656b5c3b5af4fdaefa09cf996dcf9c6e128
Carlos Henrique <114544383+CarlosH6@users.noreply.github.com>
2023-12-05 17:30:04 -0300

    Add files via upload

---
+++ memoria.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int tamanho = 5;
+  int contador;
+  int *vetor = (int *)malloc(tamanho * sizeof(int));
+  if (vetor == NULL) {
+    exit(1);
+  } else {
+    printf("Memória alocada com sucesso!\n");
+  }
+  printf("Digite os valores do vetor: \n");
+  for (contador = 0; contador < tamanho; contador++) {
+    scanf("%d", &vetor[contador]);
+  }
+  free(vetor);
+  return 0;
+}
